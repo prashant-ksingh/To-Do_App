55ef761b5faffbdc724379315aa19be66a667d56
unknown <prashantkumarsingh934@gmail.com>
2023-09-21 23:52:11 +0530

    templates done

--- todo/models.py
+++ todo/models.py
@@ -7,6 +7,7 @@ class User(models.Model):
     name =models.CharField(max_length=50)
     email = models.CharField(max_length=50)
     password = models.CharField(max_length=25)
+    address = models.CharField(max_length=100,null=True)
 
     def __str__(self):
         return f"{self.userid}-{self.name}"
